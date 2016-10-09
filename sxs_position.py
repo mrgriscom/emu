3a19667e6fb93053f7ebbafcdae56f37907e6e78
Drew Roos <mrgriscom@gmail.com>
2016-10-08 22:41:48 -0400

    3d projection math shruggie

--- sxs_position.py
+++ sxs_position.py
@@ -6,7 +6,10 @@ stretch = min(*(float(disp) / src for disp, src in zip(DISPLAY_DIM, SOURCE_DIM))
 
 xstretch = SCALE * .5 * stretch
 ystretch = SCALE * stretch
-# sep is pixels *before* xstretch is applied
+# sep unit is pixels *before* xstretch is applied
+# this formula makes both sides perfectly overlap. it is possible one might want to
+# slightly modify the separation to make it feel more natural. increasing moves the
+# scene 'into' the screen. the math is beyond me, though.
 sep = DISPLAY_DIM[0] / stretch + ((1. - SCALE) / SCALE - 1) * SOURCE_DIM[0]
 
 print 'x stretch', xstretch
