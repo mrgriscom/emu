887d94e85c0656b91b3f62fc0a172b868bf6058f
Drew Roos <mrgriscom@gmail.com>
2018-10-27 16:12:34 +0200

    retroarch shader for virtualboy sbs!

--- configs/retroarch/shaders/shaders_glsl/anaglyph-to-sbs.glsl
+++ configs/retroarch/shaders/shaders_glsl/anaglyph-to-sbs.glsl
@@ -1,123 +1,55 @@
-#define eye_sep 0.76
-#define y_loc 0.32
-#define ana_zoom 1.3
-#define WIDTH 2.4
-#define BOTH 0.52
-#define HEIGHT 2.4
-#define palette 1.0
-#define warpX 0.05
-#define warpY 0.05
+// monochrome color -- use signature red
+#define COLOR vec3(1,0,0)
+// scale down the content by this factor
+#define SCALE 1.
+// extra separation between the left- and right-eye images in units of content-widths *before* scaling
+// 0=perfect overlap; increasing moves the image into the screen, and decreasing moves out, but the math is kind of beyond me
+#define SEP 0.
+
+varying vec2 tex_coord;
 
 #if defined(VERTEX)
 
-#if __VERSION__ >= 130
-#define COMPAT_VARYING out
-#define COMPAT_ATTRIBUTE in
-#define COMPAT_TEXTURE texture
-#else
-#define COMPAT_VARYING varying 
-#define COMPAT_ATTRIBUTE attribute 
-#define COMPAT_TEXTURE texture2D
-#endif
-
-#ifdef GL_ES
-#define COMPAT_PRECISION mediump
-#else
-#define COMPAT_PRECISION
-#endif
-
-COMPAT_ATTRIBUTE vec4 VertexCoord;
-COMPAT_ATTRIBUTE vec4 COLOR;
-COMPAT_ATTRIBUTE vec4 TexCoord;
-COMPAT_VARYING vec4 COL0;
-COMPAT_VARYING vec4 TEX0;
-
-vec4 _oPosition1; 
+attribute vec2 TexCoord;
+attribute vec2 VertexCoord;
 uniform mat4 MVPMatrix;
-uniform int FrameDirection;
-uniform int FrameCount;
-uniform COMPAT_PRECISION vec2 OutputSize;
-uniform COMPAT_PRECISION vec2 TextureSize;
-uniform COMPAT_PRECISION vec2 InputSize;
 
-void main()
-{
-    vec4 _oColor;
-    vec2 _otexCoord;
-    gl_Position = VertexCoord.x * MVPMatrix[0] + VertexCoord.y * MVPMatrix[1] + VertexCoord.z * MVPMatrix[2] + VertexCoord.w * MVPMatrix[3];
-    _oPosition1 = gl_Position;
-    _oColor = COLOR;
-    _otexCoord = TexCoord.xy;
-    COL0 = COLOR;
-    vec2 shift = 0.5 * InputSize / TextureSize;
-    TEX0.xy = ((TexCoord.xy - shift) * ana_zoom + shift) * vec2(WIDTH, HEIGHT) - vec2(BOTH, 0.0);
+void main() {
+    gl_Position = MVPMatrix * vec4(VertexCoord, 0.0, 1.0);
+    tex_coord = TexCoord;
 }
 
 #elif defined(FRAGMENT)
 
-#if __VERSION__ >= 130
-#define COMPAT_VARYING in
-#define COMPAT_TEXTURE texture
-out vec4 FragColor;
-#else
-#define COMPAT_VARYING varying
-#define FragColor gl_FragColor
-#define COMPAT_TEXTURE texture2D
-#endif
-
-#ifdef GL_ES
-#ifdef GL_FRAGMENT_PRECISION_HIGH
-precision highp float;
-#else
-precision mediump float;
-#endif
-#define COMPAT_PRECISION mediump
-#else
-#define COMPAT_PRECISION
-#endif
-
-struct output_dummy {
-    vec4 _color;
-};
-
-uniform int FrameDirection;
-uniform int FrameCount;
-uniform COMPAT_PRECISION vec2 OutputSize;
-uniform COMPAT_PRECISION vec2 TextureSize;
-uniform COMPAT_PRECISION vec2 InputSize;
 uniform sampler2D Texture;
-COMPAT_VARYING vec4 TEX0;
-//standard texture sample looks like this: COMPAT_TEXTURE(Texture, TEX0.xy);
 
-//distortion
-vec2 Warp(vec2 pos){
-  pos.xy = pos.xy * 2.0-1.0;    
-  pos.xy *= vec2(1.0+(pos.y*pos.y)*warpX,1.0+(pos.x*pos.x)*warpY);
-  return pos*0.5+0.5;
+uniform vec2 OutputSize;
+uniform vec2 TextureSize;
+uniform vec2 InputSize;
+
+void main() {
+    // split into left and right virtual viewports
+    vec2 pctViewport = tex_coord / InputSize * TextureSize;
+    bool left = pctViewport.x < .5;
+    pctViewport = vec2(mod(pctViewport.x * 2., 1.), pctViewport.y);
+
+    float screen_aspect = OutputSize.x / OutputSize.y;
+    float content_aspect = InputSize.x / InputSize.y;
+    float x_stretch_to_screen = screen_aspect / content_aspect;
+
+    pctViewport -= vec2(.5, .5);
+    // resize to correct for original aspect ratio (display settings are such that it is initially stretched to fit
+    // the entire screen -- that's the only way we can draw outside the original frame)
+    pctViewport = vec2(pctViewport.x * max(x_stretch_to_screen, 1.), pctViewport.y / min(x_stretch_to_screen, 1.));
+    // separation and scaling
+    pctViewport = vec2(pctViewport.x + (left ? 1. : -1.) * SEP / 2., pctViewport.y);
+    pctViewport *= SCALE;
+    pctViewport += vec2(.5, .5);
+
+    // sample content and decode anaglpyh
+    vec4 sample = texture2D(Texture, pctViewport * InputSize / TextureSize);
+    gl_FragColor = vec4(vec3(left ? sample.r : sample.g) * COLOR, 1);
 }
 
-void main()
-{
-	output_dummy _OUT;
-	vec2 warpCoord1 = Warp((TEX0.xy - vec2(eye_sep,  y_loc))*(TextureSize.xy/InputSize.xy))*(InputSize.xy/TextureSize.xy);
-	vec2 warpCoord2 = Warp((TEX0.xy + vec2(eye_sep, -y_loc))*(TextureSize.xy/InputSize.xy))*(InputSize.xy/TextureSize.xy);
-	vec2 fragCoord1 = warpCoord1 * InputSize / TextureSize;
-	vec2 fragCoord2 = warpCoord2 * InputSize / TextureSize;
-	vec4 frame1 = vec4(0.0);
-	if ( fragCoord1.x < 1.0 && fragCoord1.x > 0.0 && fragCoord1.y < 1.0 && fragCoord1.y > 0.0 )
-		frame1 = COMPAT_TEXTURE(Texture, warpCoord1);
-	vec4 frame2 = vec4(0.0);
-	if ( fragCoord2.x < 1.0 && fragCoord2.x > 0.0 && fragCoord2.y < 1.0 && fragCoord2.y > 0.0 )
-		frame2 = COMPAT_TEXTURE(Texture, warpCoord2);
-	frame1.r = frame1.g;
-	frame2.gb = vec2(frame2.r);
-	vec4 final = vec4(0.0);
-	if (palette > 0.5)
-		final = frame1 + frame2;
-	else
-		final = vec4(frame1.r + frame2.r, 0.0, 0.0, 1.0);
-	_OUT._color = final;
-	FragColor = _OUT._color;
-	return;
-} 
 #endif
+
